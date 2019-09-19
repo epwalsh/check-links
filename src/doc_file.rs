bf90d037ff1bbb8702d3ab7ec809f717b41452cb
epwalsh <epwalsh10@gmail.com>
2019-09-19 14:09:22 -0700

    create DocFile abstraction

---
+++ src/doc_file.rs
@@ -0,0 +1,61 @@
+use std::io;
+use std::path::Path;
+
+use globset::{Glob, GlobSet, GlobSetBuilder};
+use grep_matcher::{Captures, Matcher};
+use grep_regex::RegexMatcher;
+use grep_searcher::sinks::UTF8;
+use grep_searcher::Searcher;
+
+pub struct DocFile {
+    glob_set: GlobSet,
+    pub link_matcher: RegexMatcher,
+}
+
+impl DocFile {
+    pub fn new(globs: Vec<&str>, link_pattern: &str) -> Self {
+        let mut glob_builder = GlobSetBuilder::new();
+        for glob in globs {
+            glob_builder.add(Glob::new(glob).unwrap());
+        }
+        let glob_set = glob_builder.build().unwrap();
+        let link_matcher = RegexMatcher::new(link_pattern).unwrap();
+        DocFile {
+            glob_set,
+            link_matcher,
+        }
+    }
+
+    pub fn is_match<P>(&self, p: P) -> bool
+    where
+        P: AsRef<Path>,
+    {
+        self.glob_set.is_match(p)
+    }
+
+    pub fn iter_links<P, F>(
+        &self,
+        searcher: &mut Searcher,
+        p: &P,
+        mut f: F,
+    ) -> Result<(), io::Error>
+    where
+        P: AsRef<Path>,
+        F: FnMut(u64, String) -> bool,
+    {
+        searcher.search_path(
+            &self.link_matcher,
+            p,
+            UTF8(|lnum, line| {
+                let mut captures = self.link_matcher.new_captures().unwrap();
+                self.link_matcher
+                    .captures_iter(line.as_bytes(), &mut captures, |c| {
+                        let mat = c.get(1).unwrap();
+                        let mat = line[mat].to_string();
+                        f(lnum, mat)
+                    })?;
+                Ok(true)
+            }),
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@ use std::sync::mpsc::channel;
 use std::sync::Arc;
 
 use exitfailure::ExitFailure;
-use globset::{Glob, GlobSetBuilder};
-use grep_matcher::{Captures, Matcher};
-use grep_regex::RegexMatcher;
-use grep_searcher::sinks::UTF8;
 use grep_searcher::Searcher;
 use ignore::Walk;
 use structopt::StructOpt;
 use threadpool::ThreadPool;
 
+mod doc_file;
 mod link;
 mod log;
 
+use doc_file::DocFile;
 use link::{Link, LinkStatus};
 use log::Logger;
 
@@ -42,11 +40,6 @@ fn main() -> Result<(), ExitFailure> {
     let mut logger = Logger::default(opt.verbose, !opt.no_color);
     logger.debug(&format!("{:?}", opt)[..])?;
 
-    // This is the regular expression we use to find links.
-    let matcher = RegexMatcher::new(r"\[[^\[\]]+\]\(([^\(\)]+)\)").unwrap();
-
-    let mut searcher = Searcher::new();
-
     // Initialize thread pool and channel.
     let pool = ThreadPool::new(opt.concurrency);
     let (tx, rx) = channel();
@@ -54,21 +47,25 @@ fn main() -> Result<(), ExitFailure> {
     // We'll use a single HTTP client across threads.
     let http_client = Arc::new(reqwest::Client::new());
 
+    let doc_files = vec![
+        // Rust source files.
+        DocFile::new(vec!["*.rs"], r"\[[^\[\]]+\]\(([^\(\)]+)\)"),
+        // Extra markdown files.
+        DocFile::new(vec!["*.md"], r"\[[^\[\]]+\]\(([^\(\)]+)\)"),
+    ];
+
     // We iterator through all rust and markdown files not included in your .gitignore.
-    let mut glob_builder = GlobSetBuilder::new();
-    glob_builder.add(Glob::new("*.rs")?);
-    glob_builder.add(Glob::new("*.md")?);
-    let glob_set = glob_builder.build()?;
     let file_iter = Walk::new("./")
         .filter_map(Result::ok)
         .filter(|x| match x.file_type() {
             Some(file_type) => file_type.is_file(),
             None => false,
         })
-        .map(|x| x.into_path())
-        .filter(|p| glob_set.is_match(p));
+        .map(|x| x.into_path());
 
     let mut n_links = 0;
+    let mut searcher = Searcher::new();
+
     for path in file_iter {
         let path_str = path.to_str();
         if let None = path_str {
@@ -82,34 +79,25 @@ fn main() -> Result<(), ExitFailure> {
             continue;
         }
         let path_str = path_str.unwrap();
+        for doc_file in &doc_files {
+            if doc_file.is_match(&path) {
+                logger.debug(&format!("Searching {}", path.display())[..])?;
 
-        logger.debug(&format!("Searching {}", path.display())[..])?;
-
-        searcher.search_path(
-            &matcher,
-            &path,
-            UTF8(|lnum, line| {
-                let mut captures = matcher.new_captures().unwrap();
-                matcher.captures_iter(line.as_bytes(), &mut captures, |c| {
+                doc_file.iter_links(&mut searcher, &path, |lnum, mat| {
                     n_links += 1;
-                    let m = c.get(1).unwrap();
-                    let raw = line[m].to_string();
-
-                    let mut link = Link::new(String::from(path_str), lnum as usize, raw);
-
+                    let mut link = Link::new(String::from(path_str), lnum as usize, mat);
                     let tx = tx.clone();
                     let http_client = http_client.clone();
                     pool.execute(move || {
                         link.verify(http_client);
                         tx.send(link).unwrap();
                     });
-
                     true
                 })?;
 
-                Ok(true)
-            }),
-        )?;
+                break;
+            }
+        }
     }
 
     let mut n_bad_links = 0;
